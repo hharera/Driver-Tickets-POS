aa365f46bcd39133598293e004af72b7b1162cbf
hassan shaban <61611892+hassan0shaban@users.noreply.github.com>
2021-12-27 16:09:26 +0200

    ticket actions

--- features/ticket/src/main/java/com/englizya/ticket/TicketViewModel.kt
+++ features/ticket/src/main/java/com/englizya/ticket/TicketViewModel.kt
@@ -4,6 +4,8 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.englizya.common.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import java.lang.Integer.max
+import java.lang.Integer.min
 import javax.inject.Inject
 
 @HiltViewModel
@@ -11,16 +13,24 @@ class TicketViewModel @Inject constructor(
 
 ) : BaseViewModel() {
 
-    private var _quantity = MutableLiveData<Int>()
+    private var _quantity = MutableLiveData<Int>(1)
     val quantity: LiveData<Int> = _quantity
 
 
     fun decrementQuantity() {
-        _quantity.postValue(_quantity.value!! - 1)
+        _quantity.postValue(
+            Math.max(_quantity.value!! - 1, 1)
+        )
     }
 
     fun incrementQuantity() {
-        _quantity.postValue(_quantity.value!! + 1)
+        _quantity.postValue(
+            Math.min(_quantity.value!! + 1, 99)
+        )
+    }
+
+    fun setQuantity(quantity: Int) {
+        _quantity.postValue(min(max(1, quantity), 99))
     }
 
 }
