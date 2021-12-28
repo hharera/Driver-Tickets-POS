d102bdb1718b1aebfb41bf8b65fd5d133ef01258
hassan-harera <hassan.shaban.harera@gmail.com>
2021-12-28 09:36:22 +0200

    update build gradle

--- buildSrc/src/main/java/CommonBuild.kt
+++ buildSrc/src/main/java/CommonBuild.kt
@@ -1,7 +1,8 @@
 object CommonBuild {
 
-    const val DataModule = "data-module-di.gradle"
+    const val DATA_MODULE = "data-module-di.gradle"
+    const val UI_MODULE = "ui-module-di.gradle"
     const val NavigationModule = "navigation-module-di.gradle"
     const val FeatureModule = "feature-module-di.gradle"
-    const val HiltDependencies = "feature-module-di.gradle"
+    const val HiltDependencies = "hilt-module-di.gradle"
 }
