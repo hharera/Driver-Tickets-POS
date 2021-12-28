5a54d722edbb7dcaa856bd3f681eeeb27787f24d
hassan shaban <61611892+hassan0shaban@users.noreply.github.com>
2021-12-28 08:11:47 +0200

    clean common build.gradle

--- buildSrc/src/main/java/CommonBuild.kt
+++ buildSrc/src/main/java/CommonBuild.kt
@@ -1,6 +1,7 @@
-object CommonGradle {
+object CommonBuild {
 
     const val DataModule = "data-module-di.gradle"
+    const val NavigationModule = "navigation-module-di.gradle"
     const val FeatureModule = "feature-module-di.gradle"
     const val HiltDependencies = "feature-module-di.gradle"
 }
