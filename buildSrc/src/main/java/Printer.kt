751a0ca7ea8cd400658f135a82bb435080774b11
emankhaled96 <emank5501@gmail.com>
2022-08-03 16:50:38 +0200

    update : printer version

--- buildSrc/src/main/java/Printer.kt
+++ buildSrc/src/main/java/Printer.kt
@@ -1,5 +1,5 @@
 object Printer {
 
     const val ThermalPrinter = "com.github.DantSu:ESCPOS-ThermalPrinter-Android:3.0.1"
-    const val SUNMI_PRINTER = "com.sunmi:printerlibrary:1.0.18"
+    const val SUNMI_PRINTER = "com.sunmi:printerlibrary:1.0.19"
 }
